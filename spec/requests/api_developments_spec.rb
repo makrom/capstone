e7033b8808af5cf5301fe6d1f8aae62febd69d74
makrom <makrom61@gmail.com>
2019-11-21 19:10:35 +0300

    RSpec prepared

---
+++ spec/requests/api_developments_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "ApiDevelopments", type: :request do
+  describe "RDBMS-backed" do
+    it "create RDBMS-backed model" do
+    end
+    it "expose RDBMS-backed API resource" do
+    end
+  end
+  describe "MongoDB-backed" do
+    it "create MongoDB-backed model" do
+    end
+    it "expose MongoDB-backed API resource" do
+    end
+  end
+end
