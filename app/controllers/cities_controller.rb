2f584dc1b60e17a5bfb0e54c2d711a080d978443
makrom <makrom61@gmail.com>
2019-11-22 10:14:47 +0300

    RDBMS RSpec Cities model generated

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,34 +1,26 @@
 class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :update, :destroy]
 
-  # GET /cities
-  # GET /cities.json
   def index
     @cities = City.all
-
-    render json: @cities
+    #render json: @cities
   end
 
-  # GET /cities/1
-  # GET /cities/1.json
   def show
-    render json: @city
+    #render json: @city
   end
 
-  # POST /cities
-  # POST /cities.json
   def create
     @city = City.new(city_params)
 
     if @city.save
-      render json: @city, status: :created, location: @city
+      #render json: @city, status: :created, location: @city
+      render :show, status: :created, location: @city
     else
       render json: @city.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
     @city = City.find(params[:id])
 
@@ -39,8 +31,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # DELETE /cities/1
-  # DELETE /cities/1.json
   def destroy
     @city.destroy
 
--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
   describe "RDBMS-backed" do
+    before(:each) { City.delete_all }
+    after(:each) { City.delete_all }
+
     it "create RDBMS-backed model" do
+      object=City.create(:name=>"test")
+      expect(City.find(object.id).name).to eq("test")
     end
+
     it "expose RDBMS-backed API resource" do
+      object=City.create(:name=>"test")
+      expect(cities_path).to eq("/api/cities")
+      get city_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
     end
   end
+
   describe "MongoDB-backed" do
+    before(:each) { State.delete_all }
+    after(:each) { State.delete_all }
+
     it "create MongoDB-backed model" do
     end
     it "expose MongoDB-backed API resource" do
